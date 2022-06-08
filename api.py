0f6a465891c315aab949c7fc6f1793b1170686c2
Gus Fowler <gus.fowler12@gmail.com>
2022-06-07 23:40:35 -0700

    started processing output of 'sensors' command

--- api.py
+++ api.py
@@ -1,6 +1,16 @@
+import subprocess
+
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/temps")
 def hello_world():
-    return "Hello World!\n"
+    stdOut = str(subprocess.check_output(['sensors']).decode("UTF-8"))
+    output = []
+
+    for line in stdOut.split("\n"):
+        if line != '':
+            output.append(line)
+
+    print(output)
+    return str(output)
