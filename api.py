25ed951339fa3dcae1b991cdff276d145bd8162b
Gus Fowler <gus.fowler12@gmail.com>
2022-06-08 13:48:52 -0700

    lm-sensors has it's own json output

--- api.py
+++ api.py
@@ -3,16 +3,9 @@ import subprocess
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route("/temps")
-def hello_world():
-    stdOut = str(subprocess.check_output(['sensors']).decode("UTF-8"))
-    output = []
-
-    for line in stdOut.split("\n"):
-        if line != '':
-            print(line)
-            output.append(line)
-
-
-    print(output)
-    return str(output)
+@app.route("/sensors")
+def sensors():
+    json = str(subprocess.check_output(['sensors', '-j']).decode('UTF-8'))
+    
+    print(json)
+    return json
