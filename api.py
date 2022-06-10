4f7f5bbb1678620760b4f976bcdc33e8bca75c3e
Gus Fowler <gus.fowler12@gmail.com>
2022-06-09 17:43:38 -0700

    why

--- api.py
+++ api.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route("/sensors")
 def sensors():
     json = str(subprocess.check_output(['sensors', '-j']).decode('UTF-8'))
-    
-    print(json)
+
+    return json
+
+@app.route("/liquidctl")
+def liquidctl():
+    json = str(subprocess.check_output(['/usr/local/bin/liquidctl', '--json', 'status']).decode('UTF-8'))
+
     return json
